2c94ab4f2474dad4b41c32f9465d5d98b215b4d7
AthiniLudidi <athiniludidi93@gmail.com>
2021-11-17 10:04:10 +0200

    fixed everything. Ready for unit testing

--- src/com/job_assessments/athini/Main.java
+++ src/com/job_assessments/athini/Main.java
@@ -9,105 +9,44 @@ public class Main {
 
         File userTweetsFile=new File("C:\\Users\\user\\Documents\\My_Projects\\job_assessments\\some_assessment_101\\data_files\\tweet.txt");
         File userConnectionsFile=new File("C:\\Users\\user\\Documents\\My_Projects\\job_assessments\\some_assessment_101\\data_files\\user.txt");
-        SortedMap<String, ArrayList<String>> userstweets=getUsersTweets(userTweetsFile);
 
-        System.out.println(userstweets);
 
-        SortedMap<String, ArrayList<String>> userConnectionPairs=getUserConnections(userConnectionsFile);
+        SortedMap<String, ArrayList<String>> userConnectionPairs= loadUsersAndTheirConnections(userConnectionsFile);
 
         System.out.println(userConnectionPairs);
 
-        //tweetAlgorithm(userConnectionsFile, userTweetsFile);
 
-       // tweetAlgorithmV2(userConnectionsFile, userTweetsFile);
-
-        tweetAlgorithmV3(userConnectionsFile,userTweetsFile);
+        tweetsFeedAlgorithm(userConnectionsFile,userTweetsFile);
     }
 
 
-    // main algorithm for displaying tweets
-    private static void tweetAlgorithm(File users, File tweetsFile){
-
-        Map<String, ArrayList<String>> userConnections=getUserConnections(users);
-        Map<String, ArrayList<String>> userTwits=getUsersTweets(tweetsFile);
-
-        userConnections.forEach((user, connections) -> {
-            if(userTwits.containsKey(user)) {
-                System.out.println(user);
-            }
-            connections.forEach(conn -> {
-                if(userTwits.containsKey(conn)){
-                    System.out.println(conn);
-                }
-            });
-
-            // first print all the tweets by this user
-            if (userTwits.containsKey(user)) {
-                userTwits.get(user).forEach(tweet->{
-                    System.out.println("\t@" + user + ": " + tweet);
-                });
-            }
-
-            // now print all the tweets by the users this user follows
-            connections.forEach(connection ->
-            {
-                if (userTwits.containsKey(connection)) {
-                    userTwits.get(connection).forEach(curTwit ->{
-                        System.out.println("\t@" + connection + ": " + curTwit);
-                    });
-                }
-            });
-
-        });
-
-
-    }
-
-    // v2 function for displaying the tweets- uses sorted sets
-    private static void tweetAlgorithmV2(File usersFile, File tweetsFile){
-        Map<String, ArrayList<String>> userConnections=getUserConnections(usersFile);
-        Map<String, ArrayList<String>> userTwits=getUsersTweets(tweetsFile);
-
-        userConnections.forEach((user, connections)->{
-            SortedSet<String> sortedUsers=new TreeSet<>();
-            sortedUsers.add(user);
-            sortedUsers.addAll(connections);
-
-            sortedUsers.forEach(u->{
-                if(userTwits.containsKey(u)) {
-                    System.out.println(u);
-                }
-            });
-
-            sortedUsers.forEach(curUser->{
-                if (userTwits.containsKey(curUser)) {
-                    userTwits.get(curUser).forEach(tweet->{
-                        System.out.println("\t@" + curUser + ": " + tweet);
-                    });
-                }
-            });
-        });
-
-
-    }
-
-    // v3 function for displaying the tweets- uses sorted sets
-    private static void tweetAlgorithmV3(File usersFile, File tweetsFile){
-        Map<String, ArrayList<String>> userConnections=getUserConnections(usersFile);
-        ArrayList<String> tweeters=new ArrayList<>();
-        ArrayList<String> tweets=new ArrayList<>();
-        getUsersTweetsV2(tweeters,tweets,tweetsFile);
+    /** A method to display tweets (user's tweets, and tweets from users they follow) on a user's feed in order.
+     * @param usersFile - a text file containing users and their connections (users this user follows)
+     * @param tweetsFile - a text file containing ordered users tweets
+     * */
+    private static void tweetsFeedAlgorithm(File usersFile, File tweetsFile){
+        Map<String, ArrayList<String>> userConnections= loadUsersAndTheirConnections(usersFile);
+        ArrayList<String> tweeters=new ArrayList<>(); //to hold people who tweeted
+        ArrayList<String> tweets=new ArrayList<>(); // to hold tweets corresponding to tweeters
+        loadUsersTweets(tweeters,tweets,tweetsFile);
 
         userConnections.forEach((user, connections)->{
             SortedSet<String> sortedUserConnections=new TreeSet<>();
             sortedUserConnections.add(user);
             sortedUserConnections.addAll(connections);
 
-            // first display names of users (connections) who tweeted
-            sortedUserConnections.forEach(u-> {
-                if(tweeters.contains(u))
-                    System.out.println(u);
+
+            // first display names (unique) of users (connections) who tweeted (or whose connections have tweeted)
+            SortedSet<String> sortedUsersSet=new TreeSet<>();
+            if(tweeters.contains(user))
+                sortedUsersSet.add(user);
+            connections.forEach(u->{
+                if(tweeters.contains(u)) {
+                    sortedUsersSet.add(user);
+                    sortedUsersSet.add(u);
+                }
             });
+            sortedUsersSet.forEach(System.out::println);
 
             // now display the tweets, in the order they appear
             for(int x=0; x<=tweets.size()-1; x++) {
@@ -117,48 +56,21 @@ public class Main {
         });
     }
 
-    // read the users tweets file, and populate a map/dictionary
-    private static SortedMap<String, ArrayList<String>> getUsersTweets(File file){
-        SortedMap<String, ArrayList<String>> usersTweets=new TreeMap<>();
 
-        try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
-            String line;
-            while ((line= reader.readLine())!=null){
-                //System.out.println(line);
-                String userName=line.substring(0, line.indexOf('>'));
-                String tweet=line.substring(line.indexOf('>')+2);
 
-                // ensure only twits with <= characters are saved
-                if(tweet.length()<=140){
-                    if(!usersTweets.containsKey(userName)){
-                        ArrayList<String> tweets=new ArrayList<>();
-                        tweets.add(tweet);
-                        usersTweets.put(userName,tweets);
-                    }
-                    else{
-                        ArrayList<String> tweets=usersTweets.get(userName);
-                        tweets.add(tweet);
-                        usersTweets.replace(userName,tweets);
-                    }
-                }
-
-            }
-        }catch(Exception e) {
-            e.printStackTrace();
-        }
-
-        return usersTweets;
-    }
-
-    // v2 of reading users tweets and all
-    private static void getUsersTweetsV2(ArrayList<String> tweeters, ArrayList<String> tweets, File file){
+    /** A method that reads the tweets text file into 2 lists (could not use Maps, need to have duplicates)
+     * @param tweeters - the arrayList to hold users who have twitted
+     * @param tweets - the ArrayList to hold tweets corresponding to twitters in the tweeters arrayList
+     * @param file - a text file containing ordered users tweets
+     * */
+    private static void loadUsersTweets(ArrayList<String> tweeters, ArrayList<String> tweets, File file){
         try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
             String line;
             while ((line= reader.readLine())!=null){
                 String userName=line.substring(0, line.indexOf('>'));
                 String tweet=line.substring(line.indexOf('>')+2);
 
-                // ensure only twits with <= characters are saved
+                // ensure only twits with <= 140 characters are saved
                 if(tweet.length()<=140){
                     tweeters.add(userName);
                     tweets.add(tweet);
@@ -169,8 +81,11 @@ public class Main {
         }
     }
 
-    // read the users connections file, and populate a map/dictionary
-    private static SortedMap<String, ArrayList<String>> getUserConnections(File file){
+    /** A method that loads the users (and their connections) into a Map (k=users, v=peopleTheyFollow)
+     * @param file - the text file containing users and the people they follow
+     * @return - a Map containing use-connections pairs
+     * */
+    private static SortedMap<String, ArrayList<String>> loadUsersAndTheirConnections(File file){
         SortedMap<String, ArrayList<String>> usersConnections=new TreeMap<>();
 
         try {
